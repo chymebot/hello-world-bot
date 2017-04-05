7396e47602008089d83790722ba028b81ef15f11
pradeepdey <pradeep.dey@unvired.com>
2017-04-05 17:17:14 +0530

    Upgraded to New Modeller
    Upgraded to New Modeller

--- src/com/chyme/helloworld/pa/COMMAND_HELLOWORLDSAMPLE.java
+++ src/com/chyme/helloworld/pa/COMMAND_HELLOWORLDSAMPLE.java
@@ -9,9 +9,9 @@ public class COMMAND_HELLOWORLDSAMPLE extends ABSTRACT_COMMAND_HELLOWORLDSAMPLE 
 	public void execute() {
 		chymeAPI.setTags("PA_COMMAND_HELLOWORLDSAMPLE", "");
 		try {
-			chymeAPI.setMessage(new Message("Hi! If you are seeing this message means all are working fine.", null, Message.FLAG_NO_FOLLOWUP_COMMAND));
+			chymeAPI.setMessage(new Message("Hi! If you are seeing this message means working fine.", null, Message.FLAG_NO_FOLLOWUP_COMMAND));
 		} catch (Exception e) {
-			chymeAPI.setMessage(new Message("Oops! Something not right. Please try again.", null, Message.FLAG_NO_FOLLOWUP_COMMAND));
+			chymeAPI.setMessage(new Message("Oops! Something went wrong. Please try again.", null, Message.FLAG_NO_FOLLOWUP_COMMAND));
 		}
 	}
 }
